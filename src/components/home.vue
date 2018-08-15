da33828937a2307905b40ff5de21e29a07f4e956
lufangzhou <735417031@qq.com>
2018-08-15 17:52:02 +0800

    current position

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,9 +2,19 @@
   <div class="home">
       <router-link to="/store">go store</router-link>
       <div class="amap-page-container">
-          <el-amap class="home-map" vid="amapDemo" :amap-manager="amapManager" :center="center" :zoom="zoom" :plugin="plugin" :events="events" >
+          <el-amap class="home-map"
+                   vid="amapDemo"
+                   :amap-manager="amapManager"
+                   :center="center"
+                   :zoom="zoom"
+                   :plugin="plugin"
+          >
           </el-amap>
+          <div class="toolbar">
+             <!-- <button @click="getMap()">get map</button>-->
+          </div>
       </div>
+
   </div>
 </template>
 <script>
@@ -16,41 +26,43 @@
                 amapManager: new this.$VueAMap.AMapManager(),
                 zoom: 12,
                 center: [121.59996, 31.197646],
-                plugin: ['ToolBar', {
-                    pName: 'MapType',
-                    defaultType: 0,
+                plugin: [{
+                    pName: 'Geolocation',
                     events: {
                         init(o) {
-                            console.log(o);
+                            // o 是高德地图定位插件实例
+                            o.getCurrentPosition((status, result) => {
+                                 console.log(35,result);  //  能获取定位的所有信息
+                                if (result && result.position) {
+                                    self.str = result.formattedAddress;
+                                    console.log(38,self.str);
+                                    /*self.positions.address = self.str.substring(self.str.indexOf('市') + 1);
+                                    self.positions.lng = result.position.lng;
+                                    self.positions.lat = result.position.lat;
+                                    self.positions.loaded = true;
+                                    self.$nextTick();
+                                    // 把获取的数据提交到 store 的数据中，以便其他单文件组件使用
+                                    self.$store.commit('POSITION', self.positions);
+                                    // console.log(self.positions);
+                                    sessionStorage.setItem('id', JSON.stringify(self.positions));*/
+                                }
+                            });
                         }
                     }
                 }],
-                events: {
-                    init: (o) => {
-                        //console.log(o.getCenter())
-                        //console.log(this.$refs.map.$$getInstance())
-                        o.getCity(result => {
-                            console.log(result)
-                        })
-                    },
-                    'moveend': () => {
-                    },
-                    'zoomchange': () => {
-                    },
-                    'click': (e) => {
-                        console.log('点击');
-                        //alert('map clicked');
-                    }
-                },
+                position:{
+                    address:'',
+                    lnglatXY:[],
+                    lng: '',
+                    lat: ''
+                }
             }
         },
         methods:{
-            getPosition(){
 
-            }
         },
         created(){
-            this.getPosition();
+           // this.getPosition();
         }
 
     }
--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import remInit from '@/assets/js/remInit'
 import usePrototype from '@/assets/js/prototype'
 // 引入vue-amap
 import VueAMap from 'vue-amap';                                //高德地图
+import { lazyAMapApiLoaderInstance } from 'vue-amap';
 
 import Vant from 'vant';
 import 'vant/lib/vant-css/index.css';
@@ -24,10 +25,27 @@ VueAMap.initAMapApiLoader({
     // 高德的key
     key: '6e9a0349872b8b0f2923e901f94d0e2e',
     // 插件集合
-    plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor'],
+    plugin: [
+        'AMap.Autocomplete',
+        'AMap.PlaceSearch',
+        'AMap.Scale',
+        'AMap.OverView',
+        'AMap.ToolBar',
+        'AMap.MapType',
+        'AMap.PolyEditor',
+        'AMap.CircleEditor',
+        'AMap.Geolocation'
+    ],
+    //uiVersion: '1.0', // ui库版本，不配置不加载,
     // 高德 sdk 版本，默认为 1.4.4
     v: '1.4.4'
 });
+/*lazyAMapApiLoaderInstance.load().then(() => {
+    // your code ...
+    this.map = new AMap.Map('amapContainer', {
+        center: new AMap.LngLat(121.59996, 31.197646)
+    });
+});*/
 
 /* eslint-disable no-new */
 new Vue({
