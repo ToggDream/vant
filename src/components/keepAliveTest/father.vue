0471f1adbbcd11bf724d7b99f81724e9a899cb34
lufangzhou <735417031@qq.com>
2018-12-28 17:15:07 +0800

    路由嵌套

--- src/components/keepAliveTest/father.vue
+++ src/components/keepAliveTest/father.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="p-father">
         <router-link :to="i.path" v-for="(i,index) in nav"
+            :key="index"
             :class="currentIndex === index?'actived':''"
             @click="currentIndex = index"
         >{{i.name}}</router-link>
@@ -19,8 +20,8 @@
     data(){
       return {
         nav: [
-          {name:'内容一', path: 'child_1'},
-          {name:'内容二', path: 'child_2'}
+          {name:'内容一', path: '/father/child_1'},
+          {name:'内容二', path: '/father/child_2'}
         ],
         currentIndex: 0
       }
