f410d15fadc33bed106025c5716c3ecec18c3dfd
zhoufanglu <735417031@qq.com>
2019-03-19 10:49:13 +0800

    async await 头晕目眩

--- src/components/jsAsync.vue
+++ src/components/jsAsync.vue
@@ -13,23 +13,59 @@
       init(){
         //电影介绍
         //https://suggest.taobao.com/sug?code=utf-8&q=%E6%89%8B%E6%9C%BA
-        this.$fetch('/api/sug?code=utf-8&q=手机',)
-          .then((res)=>{
-            console.log('1',res.result[0]);
+        /*this.$fetch('/api/sug?code=utf-8&q=手机',)
+          .then((res) => {
+            console.log('1', res.result[0]);
             this.$fetch('/api/sug?code=utf-8&q=包')
-              .then((res)=>{
-                console.log('2',res.result[0]);
+              .then((res) => {
+                console.log('2', res.result[0]);
                 this.$fetch('/api/sug?code=utf-8&q=水')
-                  .then((res)=>{
-                    console.log('3',res.result[0]);
+                  .then((res) => {
+                    console.log('3', res.result[0]);
                   })
               })
-        })
-          /*.then((res)=>{this.$fetch('/api/sug?code=utf-8&q=包')
-          .then((res)=>{})
-        }).then((res)=>{
+          })*/
+        const getA = ()=>{
+          this.$fetch('/api/sug?code=utf-8&q=手机',)
+            .then((res)=>{
+              console.log('1', res.result[0]);
+            })
+        }
 
-        })*/
+        const getB = ()=>{
+          this.$fetch('/api/sug?code=utf-8&q=包',)
+            .then((res)=>{
+              console.log('2', res.result[0]);
+            })
+        }
+
+        const getC = ()=>{
+          this.$fetch('/api/sug?code=utf-8&q=水',)
+            .then((res)=>{
+              console.log('3', res.result[0]);
+            })
+        }
+        /*console.time('get1');
+        getA()
+        getB()
+        getC()
+        console.timeEnd('get1')*/
+        async function load() {
+          await getA()
+          await getB()
+          await getC()
+        }
+        console.time('get')
+        load()
+        console.timeEnd('get')
+
+        async function getABC() {
+          let results = await Promise.all([ getA(),getB(),getC()])
+          return results.reduce((total,value) => total*value)
+        }
+        /*console.time('async')
+        getABC()
+        console.timeEnd('async')*/
       },
       callbackTest(){
         /**
@@ -63,12 +99,39 @@
         masterFn(function say(data){
           console.log(data)
         })//输出name*/
-      }
+      },
+      asyncAwait(){
+
+        function fnAfter2Second(res) {
+          new Promise(resolve => {
+            setTimeout(()=>{
+              console.log(res);
+            },2000)
+          })
+        }
+
+        async function test(){
+          await fnAfter2Second('666')
+          console.log('calling');
+        }
+
+        test()
+      },
+      test(){
+       async function f(){
+         return await 123
+       }
+       f().then(res =>{
+         console.log(res);
+       })
+      },
     },
     mounted(){
       //callback
       //this.callbackTest()
-      this.init()
+      //this.init()
+      //this.asyncAwait()
+      this.test()
     }
   }
 </script>
